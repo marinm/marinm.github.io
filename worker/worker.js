c67724d8683d19e3e77780b5dac78895e6648bf0
Marin <marinmema@live.ca>
2025-04-11 18:58:39 -0400

    add run,pause,reset buttons

--- worker/worker.js
+++ worker/worker.js
@@ -1,41 +1,27 @@
-const MAX_SAFE_WORK_ITERATIONS = 1e6;
-
-let working = false;
-let timerOn = false;
-
 self.addEventListener("message", function (event) {
-    working = true;
-    const t0 = performance.now();
-
-    const counter = timeLimitedWork(1000);
-
-    const t1 = performance.now();
-    const elapsed = parseInt(t1 - t0);
-    working = false;
-
-	self.postMessage(`worked for ${Intl.NumberFormat("en-CA").format(elapsed)} ms, counted to ${Intl.NumberFormat("en-CA").format(counter)}`);
+    switch (event.data) {
+        case "run":
+            run();
+            break;
+        case "pause":
+            pause();
+            break;
+        case "reset":
+            reset();
+            break;
+        default:
+            self.postMessage("received unknown message");
+    }
 });
 
-function shouldWork() {
-    return timerOn;
+function run() {
+    self.postMessage("received run message");
 }
 
-function timeLimitedWork(timeLimit) {
-    timerOn = true;
-
-    // The callback gets queued but is not run at the expected time.
-    setTimeout(() => (timerOn = false), timeLimit);
-
-    // work() keeps control until it reaches MAX_SAFE_WORK_ITERATIONS.
-    return work();
+function pause() {
+    self.postMessage("received pause message");
 }
 
-function work() {
-    let counter = 0;
-
-    while (shouldWork() && counter < MAX_SAFE_WORK_ITERATIONS) {
-        counter++;
-    }
-
-    return counter;
+function reset() {
+    self.postMessage("received reset message");
 }
