48814e5f8a73478f3cee35753d8e25c626d12fad
Marin <marinmema@live.ca>
2025-05-04 13:33:27 -0400

    use swap function

--- insertion-sort/insertion-sort.js
+++ insertion-sort/insertion-sort.js
@@ -5,18 +5,20 @@ export async function insertionSort(values, interrupt) {
     for (let i = 1; i < values.length; i++) {
         // Starting at i, going backwards, stopping at the first value...
         for (let j = i; j > 0; j--) {
-            const vCurrent = values[j];
-            const vPrev = values[j - 1];
-
-            if (vCurrent < vPrev) {
-                // Swap
-                values[j] = vPrev;
-                values[j - 1] = vCurrent;
-
+            if (values[j] < values[j - 1]) {
+                swap(values, j, j - 1);
                 await interrupt();
             } else {
                 break;
             }
         }
     }
+}
+
+function swap(values, i, j) {
+    const iValue = values[i];
+    const jValue = values[j];
+
+    values[i] = jValue;
+    values[j] = iValue;
 }
